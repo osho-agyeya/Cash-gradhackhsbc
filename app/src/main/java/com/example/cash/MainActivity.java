75fdb65243706a4641a03e53bf5ec65d83c67099
Osho Agyeya <oshoagyeya123@gmail.com>
2019-09-27 12:11:24 +0530

    done once again

--- app/src/main/java/com/example/cash/MainActivity.java
+++ app/src/main/java/com/example/cash/MainActivity.java
@@ -48,40 +48,6 @@ public class MainActivity extends AppCompatActivity {
                         Log.e("TTS", "Error in converting Text to Speech!");
                     }
                 } else {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                     Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
                 }
             }
